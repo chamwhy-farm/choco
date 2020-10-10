af411c3269b2668dbd962fc8b17213c0c25b05b5
cham_why <53082717+chamwhy@users.noreply.github.com>
2020-10-10 13:44:46 +0900

    update

--- orders/attendance.js
+++ orders/attendance.js
@@ -15,24 +15,24 @@ const config = require('../config.json');
 
 
 const attendanceUser = async (msg, word) => {
+    let answer = "";
     let user = await User.findOne({userID: msg.author.id});
     if(!user){
         console.log("create new user! name: " + msg.author.username);
         user = await createUser(msg.author.id);
     }
     if(user.attendance.indexOf(moment().startOf('day').toDate().getTime()) != -1){
-        msg.reply("이미 출석하셨습니다.");
-        return {
-            attendanceCanvas: null,
-            answer: null
-        };
+        
+        answer = "이미 출석하셨습니다.";
+    }else{
+        //user 수정
+        user.addAttend(moment().startOf('day').toDate().getTime());
+        user.addChoco(50);
+        user.save();
     }
     console.log(user.attendance);
 
-    //user 수정
-    user.addAttend(moment().startOf('day').toDate().getTime());
-    user.addChoco(50);
-    user.save();
+    
 
 
     console.log(user.attendance);
@@ -42,7 +42,7 @@ const attendanceUser = async (msg, word) => {
     const ctx = canvas.getContext('2d');
     ctx.font = '30px Impact';
 
-    ctx.fillStyle = "#55352b";
+    ctx.fillStyle = config.color;
     ctx.fillRect(0, 0, 720, 720);
     const startOfMonth = moment().startOf('month').day()+1;
     const endOfMonth   = moment().endOf('month').day() + 29;
@@ -62,7 +62,7 @@ const attendanceUser = async (msg, word) => {
                 ctx.fillStyle = "gray";
             }else{
                 if(user.attendance.indexOf(thisDay) != -1){
-                    ctx.fillStyle = config.color;
+                    ctx.fillStyle = config.whiteColor;
                 }else{
                     ctx.fillStyle = "white";
                 }
@@ -86,17 +86,12 @@ const attendanceUser = async (msg, word) => {
             }
             
             createRoundBox(ctx, 10, 80, 80, i*100+20, j*100+20+100, text, textColor);
-            ctx.closePath();
-            ctx.moveTo(i*100+20, j*100+120);
-            ctx.beginPath();
-            ctx.fillStyle = "#55352b";
-            ctx.arc(i*100+60, j*100+160, 30, 0, Math.PI*2);
-            ctx.fill();
-            ctx.closePath();
+            
         }
     }
-
-    const answer = `${50}초코를 획득하셨습니다!`;
+    if(answer == ""){
+        answer = `${50}초코를 획득하셨습니다!`;
+    }
     return {attendanceCanvas: canvas, answer: answer};
 };
 
